a2f4789259c08a46a8d0730a8274d14d1f907183
vinicuis.sibin <ra105482@uem.br>
2019-05-07 08:10:42 -0300

    Feito a parte de adm no trabalho de POO

--- Programas/workspace/Detetive/src/br/com/uem/din/poo/detetive/administrador/Administrador.java
+++ Programas/workspace/Detetive/src/br/com/uem/din/poo/detetive/administrador/Administrador.java
@@ -9,24 +9,54 @@ public class Administrador {
 			System.out.println("ERRO!\nSenha incorreta.");
 			System.exit(0);
 		}
-		
-		System.out.println("Você logou como adminstrador.\n"
-							+ "O que pode fazer como Adm?\n "
-							+ "Você pode Criar, recuperar, alterar ou excluir algum objeto do jogo.\n"
-							+ "O que deseja fazer\n?"
-							+ "Digite a Ação + objeto\n");
-		
-		String opcao = scan.nextLine();
-		switch (opcao.intern()) {
-		case :
-			
-			break;
 
-		default:
-			break;
+		System.out.println("Você logou como adminstrador.\n" + "O que pode fazer como Adm?\n "
+				+ "Você pode Criar, recuperar, alterar ou excluir algum objeto do jogo.\n" 
+				+ "O que deseja fazer\n?"
+				+ "Digite a Ação + objeto\n");
+
+		String opcao = scan.nextLine();
+		String[] separa = opcao.split(" ");
+
+		if (separa[1].intern() == "criar" && separa[2].intern() == "suspeito") {
+			System.out.println("Deu certo");
+
+		} else if (separa[1].intern() == "criar" && separa[2].intern() == "arma") {
+			System.out.println("Deu certo");
+
+		} else if (separa[1].intern() == "criar" && separa[2].intern() == "local") {
+			System.out.println("Deu certo");
 		}
-		
-		
-		
+
+		else if (separa[1].intern() == "recuperar" && separa[2].intern() == "suspeito") {
+			System.out.println("Deu certo");
+
+		} else if (separa[1].intern() == "recuperar" && separa[2].intern() == "arma") {
+			System.out.println("Deu certo");
+
+		} else if (separa[1].intern() == "recuperar" && separa[2].intern() == "local") {
+			System.out.println("Deu certo");
+		}
+
+		else if (separa[1].intern() == "alterar" && separa[2].intern() == "suspeito") {
+			System.out.println("Deu certo");
+
+		} else if (separa[1].intern() == "alterar" && separa[2].intern() == "arma") {
+			System.out.println("Deu certo");
+
+		} else if (separa[1].intern() == "alterar" && separa[2].intern() == "local") {
+			System.out.println("Deu certo");
+		}
+
+		else if (separa[1].intern() == "excluir" && separa[2].intern() == "suspeito") {
+			System.out.println("Deu certo");
+
+		} else if (separa[1].intern() == "excluir" && separa[2].intern() == "arma") {
+			System.out.println("Deu certo");
+
+		} else if (separa[1] == "excluir" && separa[2] == "local") {
+			System.out.println("Deu certo");
+		}
+
 	}
 }
